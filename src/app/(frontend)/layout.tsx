0fd73d97cbb548d5ea248f68614aeb1ff4123440
Martín Stanicio <martuytimo@gmail.com>
2025-12-17 02:00:45 +0000

    add default metadata for all pages

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,10 @@
 import '@/globals.css'
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export { metadata } from '@/lib/metadata'
+
+type Props = { children: React.ReactNode }
+
+export default async function RootLayout({ children }: Props) {
   return (
     <html lang="es">
       <body>
---
+++ src/lib/metadata.ts
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next'
+
+export const siteName = 'CONCESIONARIA'
+export const title = 'Venta de autos nuevos y usados en CIUDAD'
+export const description =
+  'Encontrá tu próximo auto en CONCESIONARIA. Amplio stock de usados seleccionados y 0km. Tomamos tu usado, ofrecemos financiación y gestoría. ¡Visitanos en CIUDAD!'
+export const keywords = [
+  'concesionaria',
+  'venta de autos',
+  'autos nuevos',
+  'autos usados',
+  '0km',
+  'agencia de autos',
+  'compra venta automotor',
+  'escobar',
+  'permutas',
+  'financiacion',
+]
+// export const url = new URL('https://www.concesionaria.com/')
+
+export const metadata: Metadata = {
+  // metadataBase: url,
+  title: {
+    template: `%s | ${siteName}`,
+    default: `${title} | ${siteName}`,
+  },
+  description,
+  keywords,
+  creator: 'Martín Stanicio',
+  generator: 'Next.js',
+  openGraph: {
+    siteName,
+    title,
+    description,
+    type: 'website',
+    locale: 'es',
+    // url,
+  },
+}
