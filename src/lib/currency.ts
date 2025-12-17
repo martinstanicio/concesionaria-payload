80371cafe02a7718762564ea007dc2632bded3f5
Martín Stanicio <martuytimo@gmail.com>
2025-12-17 19:09:11 +0000

    add currency formatting utils

---
+++ src/lib/currency.ts
@@ -0,0 +1,11 @@
+export const ARS = new Intl.NumberFormat('es-AR', {
+  style: 'currency',
+  currency: 'ARS',
+  maximumFractionDigits: 0,
+})
+
+export const USD = new Intl.NumberFormat('es-AR', {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 0,
+})
