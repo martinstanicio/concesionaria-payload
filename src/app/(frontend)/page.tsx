fd52fd134e20b8006c2bded939fd9f4fe532594e
Martín Stanicio <martuytimo@gmail.com>
2025-12-18 18:31:06 +0000

    create FeaturedVehicles component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,9 +1,13 @@
+import FeaturedVehicles from '@/components/featured-vehicles'
 import Hero from '@/components/hero'
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
+      <main>
+        <FeaturedVehicles />
+      </main>
     </div>
   )
 }
---
+++ src/components/featured-vehicles.tsx
@@ -0,0 +1,48 @@
+import { Button } from './ui/button'
+import VehicleCard from './vehicle-card'
+import { cn } from '@/lib/utils'
+import config from '@/payload.config'
+import Link from 'next/link'
+import { getPayload } from 'payload'
+
+type Props = React.ComponentProps<'section'>
+
+export default async function FeaturedVehicles(props: Props) {
+  const payload = await getPayload({ config })
+  const vehicles = await payload.find({
+    collection: 'vehicles',
+    depth: 1,
+    limit: 3,
+  })
+
+  if (vehicles.totalDocs < 1) return
+
+  return (
+    <section {...props}>
+      <div className="container mx-auto space-y-12 px-4 py-24">
+        <div className="space-y-2 sm:text-center">
+          <h2 className="text-3xl font-bold">Vehículos destacados</h2>
+          <p className="text-muted-foreground">
+            Nuestra selección de vehículos destacados, elegidos por su calidad y popularidad.
+          </p>
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-8">
+          {vehicles.docs.map((vehicle) => (
+            <VehicleCard key={vehicle.id} vehicle={vehicle} className="basis-md" />
+          ))}
+        </div>
+
+        <div className="flex justify-center">
+          <Button
+            size="lg"
+            asChild
+            className={cn(vehicles.totalDocs > 1 ? 'max-lg:w-full' : 'max-sm:w-full')}
+          >
+            <Link href="/vehiculos">Ver vehículos</Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+}
