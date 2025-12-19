e7dcdfd518553494d71dfe767e97c0964a39c258
Martín Stanicio <martuytimo@gmail.com>
2025-12-19 20:27:28 +0000

    wrap VehiclesCatalogue component in a div for consistency

--- src/components/vehicles-catalogue.tsx
+++ src/components/vehicles-catalogue.tsx
@@ -2,18 +2,26 @@ import PaginationBar from './pagination-bar'
 import SearchBar from './search-bar'
 import SortingBar from './sorting-bar'
 import VehiclesGrid from './vehicles-grid'
+import { cn } from '@/lib/utils'
 import config from '@/payload.config'
 import { getPayload } from 'payload'
 import { Suspense } from 'react'
 
-type Props = {
+type Props = React.ComponentProps<'div'> & {
   page: number
   sort: string
   search: string
   limit?: number
 }
 
-export default async function VehiclesCatalogue({ page, sort, search, limit = 12 }: Props) {
+export default async function VehiclesCatalogue({
+  page,
+  sort,
+  search,
+  limit = 12,
+  className,
+  ...props
+}: Props) {
   const payload = await getPayload({ config })
   const vehicles = await payload.find({
     collection: 'vehicles',
@@ -27,7 +35,7 @@ export default async function VehiclesCatalogue({ page, sort, search, limit = 12
   })
 
   return (
-    <>
+    <div className={cn('space-y-6', className)} {...props}>
       <div className="flex flex-col gap-4 md:flex-row">
         <Suspense>
           <SearchBar className="flex-1" />
@@ -42,6 +50,6 @@ export default async function VehiclesCatalogue({ page, sort, search, limit = 12
       <VehiclesGrid vehicles={vehicles.docs} />
 
       <PaginationBar totalPages={vehicles.totalPages} currentPage={vehicles.page || +page} />
-    </>
+    </div>
   )
 }
