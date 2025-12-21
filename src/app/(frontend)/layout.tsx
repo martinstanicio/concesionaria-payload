709efed5ee9ce6c892771fc9fe0fe988a9923362
Martín Stanicio <martuytimo@gmail.com>
2025-12-21 14:57:50 +0000

    setup vercel speed insights

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,6 +2,7 @@ import { Footer } from '@/components/footer'
 import { Navbar } from '@/components/navbar'
 import '@/globals.css'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export { metadata, viewport } from '@/content/metadata'
 
@@ -15,6 +16,7 @@ export default async function RootLayout({ children }: Props) {
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
